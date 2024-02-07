60d10a803a6f870393886f5b88a21a7806c0fae9
karlottobenz <157218630+karlottobenz@users.noreply.github.com>
2024-02-07 17:59:44 +0100

    Add files via upload
    Riscritto la funzione senza utilizzare la libreria vector

--- visualizza_classifica.cpp
+++ visualizza_classifica.cpp
@@ -1,5 +1,4 @@
 #include <ncurses.h>
-#include <vector>
 #include <string>
 #include <fstream>
 #include "visualizza_classifica.hpp"
@@ -7,51 +6,47 @@
 using std::getline;
 using std::ifstream;
 using std::string;
-using std::vector;
 
-
-
-void visualizza_classifica(){
-
-    /* AVVIO NCURSES*/
+void visualizza_classifica() {
+    // Avvio ncurses
     initscr();
     noecho();
     refresh();
 
     int larghezza;
-    vector<string> righe = readFile("classifica.txt", larghezza);
+    const int MAX_RIGHE = 100;  // Numero massimo di righe da leggere
+    string righe[MAX_RIGHE];  // Array per memorizzare le righe del file
+    int num_righe = 0;  // Numero effettivo di righe lette
 
-    WINDOW* win_classifica = newwin(righe.size() + 2, larghezza + 2, 5, 15);
+    num_righe = readFile("classifica.txt", righe, MAX_RIGHE, larghezza);
+
+    WINDOW* win_classifica = newwin(num_righe + 2, larghezza + 2, 5, 15);
     box(win_classifica, 0, 0);
 
-    for(int i = 0; i < righe.size(); i++){
-        mvwprintw(win_classifica, i+1, 1, righe[i].c_str());
+    for (int i = 0; i < num_righe; i++) {
+        mvwprintw(win_classifica, i + 1, 1, righe[i].c_str());
     }
 
     wgetch(win_classifica);
 
     endwin();
-
-
 }
 
-vector<string> readFile(string nomefile, int &larghezza_massima){
-
+// Funzione per leggere il file e memorizzare le righe in un array
+int readFile(string nomefile, string righe[], int max_righe, int &larghezza_massima) {
     ifstream file(nomefile);
-    vector<string> righe;
     string riga;
     int piu_lunga = 0;
+    int num_righe = 0;
 
-    while (getline(file, riga))
-    {
-        righe.push_back(riga);
-        if(riga.length() > piu_lunga){
-
+    while (getline(file, riga) && num_righe < max_righe) {
+        righe[num_righe++] = riga;
+        if (riga.length() > piu_lunga) {
             piu_lunga = riga.length();
         }
     }
 
     larghezza_massima = piu_lunga;
 
-    return righe;
+    return num_righe;
 }
--- visualizza_classifica.hpp
+++ visualizza_classifica.hpp
@@ -1,12 +1,12 @@
 #include <ncurses.h>
-#include <vector>
+
 #include <string>
 #include <fstream>
 
 using std::getline;
 using std::ifstream;
 using std::string;
-using std::vector;
+
 
 void visualizza_classifica();
-vector<string> readFile(string, int &);
+int readFile(string nomefile, string righe[], int max_righe, int &larghezza_massima);
